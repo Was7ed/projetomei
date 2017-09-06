47d772ff894d531a6505780775a6c76870332742
Was7ed <dimitrirocha@ymail.com>
2017-09-05 22:23:10 -0300

    Criação da navbar e sessão 1
    Preciso ainda pegar a imagem de background da primeira sessão, a fonte o logo e as cores corretas.
    Fazer depois disso as outras sessões

---
+++ _ref/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt_br">
+<head>
+	<meta name="author" content="Praxis Empresa Jr.">
+	<meta charset="UTF-8">
+	<meta name="keywords" content="gestao, gestão, MEI, consultoria">
+	<link rel="stylesheet" href="_css/style.css">
+	<title>Consultoria MEI</title>
+</head>
+<body>
--- index.php
+++ index.php
@@ -1,1 +1,34 @@
-alvin puta 
+<?php 
+	require_once '_ref/header.php';
+ ?>
+
+	<header>
+		<nav>
+			<div>
+				<h1>logo</h1>
+			</div>
+			<ul>
+				<li>HOME</li>
+				<li>NOSSA EMPRESA</li>
+				<li>NOVIDADES</li>
+				<li>Cadastre-se</li>
+				<li>Entrar</li>
+			</ul>
+		</nav>
+
+	</header>
+
+	<section id="primeiro">
+		<div id="img-front">
+
+			<h2>A melhor resposta para sua micro ou pequena empresa</h2>
+
+			<h3>Você tem dúvidas? Nós respondemos</h3>
+
+			<button id="btn-mais">SAIBA MAIS</button>
+
+		</div>		
+	</section>
+
+</body>
+</html>
