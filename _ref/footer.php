c7c726bf4a28c9d3d8c8e4243f2da465fe221eff
Was7ed <dimitrirocha@ymail.com>
2017-10-30 18:21:07 -0200

    Nova navbar colocada
    Alguns ajustes precisam ser feitos na nova navbar.
    - O ícone responsivo só aparece no centro
    - Ela não está fixa
    - O logo fica mal colocado quando ela está no desktop
    - O <main> está mudando a cor do background inteiro da página

--- _ref/footer.php
+++ _ref/footer.php
@@ -1,7 +1,11 @@
-	
+
+  </main>
 	<script src="_media/jquery-3.2.1.js"></script>
 	<script src="_media/OwlCarousel2-2.2.1/dist/owl.carousel.min.js"></script>
 	<script src="_js/javascript.js"></script>
 	<script src="_js/sidemenu.js"></script>
+	<script src="_js/jquery-2.1.1.js"></script>
+	<script src="_js/jquery.mobile.custom.min.js"></script>
+	<script src="_js/navbar-main.js"></script> <!-- Resource jQuery -->
 </body>
 </html>
