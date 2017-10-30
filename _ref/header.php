3bd42cc9d55459a49e201236b3bc89d10443b242
Was7ed <dimitrirocha@ymail.com>
2017-10-30 16:24:09 -0200

    Teste Merge

--- _ref/header.php
+++ _ref/header.php
@@ -11,4 +11,42 @@
 	<link rel="stylesheet" href="_css/style_responsive.css">
 	<title>Consultoria MEI</title>
 </head>
+<<<<<<< HEAD
 <body>
+=======
+<body>
+
+
+	<header class="main">
+		<nav>
+			<div>
+				<img src="_img/logo.png" alt="">
+			</div>
+			<ul>
+				<a href="index.php"><li>HOME</li></a>
+				<a href="#"><li>NOSSA EMPRESA</li></a>
+				<a href="#novidades"><li>NOVIDADES</li></a>
+				<a href="cadastro.php"><li>Cadastre-se</li></a>
+				<a href="entrar.php"><li>Entrar</li></a>
+			</ul>
+			<span class="hidden" id="Open">
+				<a href="" onclick="openSideMenu()">
+					<svg width="30" height="30">
+           			 	<path d="M0,5 30,5" stroke="#fff" stroke-width="5"/>
+          			  	<path d="M0,14 30,14" stroke="#fff" stroke-width="5"/>
+          				<path d="M0,23 30,23" stroke="#fff" stroke-width="5"/>
+		        	</svg>
+				</a>
+			</span>
+			<div class="hidden" id="side_menu">
+				<a href="#" onclick="closeSideMenu()">&times;</a>
+				<a href="index.php"><li>HOME</li></a>
+				<a href="#"><li>NOSSA EMPRESA</li></a>
+				<a href="#novidades"><li>NOVIDADES</li></a>
+				<a href="#"><li>Cadastre-se</li></a>
+				<a href="#"><li>Entrar</li></a>
+			</div>
+		</nav>
+
+	</header>
+>>>>>>> parent of b343435... Tentativa de colocar o bootstrap
