95218b9f9abec5d8926130b71ac8d50ac14f871c
Was7ed <dimitrirocha@ymail.com>
2017-10-30 15:58:42 -0200

    Inicio PHP

--- cadastro.php
+++ cadastro.php
@@ -1,18 +1,19 @@
 <?php 
 	require_once '_ref/header.php';
+	require_once '_ref/sign_up_backend.php'
  ?>
 
 <section id="cadastro_container">
  <div id="cadastro_master">
  	<h1>CADASTRAR-SE</h1>
- 	<form method="post" action="">
+ 	<form method="post" action="_ref/sign_up_backend.php">
  		<input type="text" name="login_cadastro" placeholder="Login" required><br>
  		<input required type="password" name="senha_cadastro" placeholder="Senha"><br>
  		<input type="email" name="email_cadastro" placeholder="Email"><br>
  		<input type="number" name="cnpj_cadastro" placeholder="Digite seu CNPJ"><br>
  		<input type="text" name="prof_cadastro" placeholder="Sua profissão"><br>
  		<input type="text" name="address_cadastro" placeholder="Seu enrereço"><br>
- 		<button type="submit">Enviar</button>
+ 		<button type="submit" name="btn-register">Enviar</button>
  	</form>
  </div>
 </section>
