a78b7a990a6e1b2a8b4f928edabbd15036b4302d
SantosGustavo99 <31632805+SantosGustavo99@users.noreply.github.com>
2017-11-29 15:20:14 -0300

    com front

---
+++ _js/sidemenu.js
@@ -0,0 +1,9 @@
+function openSideMenu(){
+	document.getElementById('side_menu').style.width = "250px";
+
+}
+
+function closeSideMenu(){
+	document.getElementById('side_menu').style.width = "0";
+	document.getElementById('side_menu').style.marginLeft = "250px";
+}
